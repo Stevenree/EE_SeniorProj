13b6359c71ae37fdb7b36d4dc0308d2e1253ad8c
Thodori Kapouranis <thodorikap@gmail.com>
2021-10-19 15:39:15 -0400

    move logic for fileSelector
    need to figure out how to make directory work...

--- frontend/src/Pages/Reader/Reader.tsx
+++ frontend/src/Pages/Reader/Reader.tsx
@@ -7,11 +7,35 @@ import ChildMenuButton from 'src/global-components/MenuButtons/ChildMenuButton'
 import TopMenuButton from 'src/global-components/MenuButtons/TopMenuButton'
 export default class Reader extends Component {
     
+    comic_folder_url:string = ""
+    fileSelector:HTMLInputElement = document.createElement('input')
+
+    componentDidMount(){
+        // initialize the input element
+        this.fileSelector.setAttribute('type', 'file');
+        this.fileSelector.setAttribute('name', 'fileList');
+        this.fileSelector.setAttribute('webkitdirectory', 'multiple');
+        this.fileSelector.setAttribute('multiple', 'multiple');
+
+        this.fileSelector.onchange = (e) => {alert("change event")}
+
+        this.fileSelector.addEventListener('change', e=>{
+            alert("CHANGE EVENT")
+            let files = (e.target as HTMLInputElement).files
+            alert(files);
+            // for (let file of Array.from(files)) {
+            //     alert(file)
+                // let item = document.createElement('li');
+                // item.textContent = file.webkitRelativePath;
+                // listing.appendChild(item);
+            // };
+        })
+    }
+
+
     selectFiles = () => {
-        const fileSelector = document.createElement('input');
-        fileSelector.setAttribute('type', 'file');
-        fileSelector.setAttribute('multiple', 'multiple');
-        fileSelector.click();
+        this.fileSelector.value="";
+        this.fileSelector.click();
     }
 
     render() {
