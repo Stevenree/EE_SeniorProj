f3fb7a524e93f35c90612db8ac6e536ec6b2ae94
Thodori Kapouranis <thodorikap@gmail.com>
2021-10-23 19:35:08 -0400

    Changed Reader to function component and not class component
    we are in the modern age now boys

--- frontend/src/Pages/Reader/Reader.tsx
+++ frontend/src/Pages/Reader/Reader.tsx
@@ -9,72 +9,62 @@ import useKeyboardShortcut from './use-keyboard-shortcut'
 
 declare var window: any;
 
-type ReaderState = {
-    image_urls: string[]|null,
-    total_images: number
-}
-export default class Reader extends Component {
 
-    state:ReaderState = {
-        image_urls: null,
-        total_images: 0
-    }
+export default function Reader() {
+    const [page_urls, setUrls] = React.useState([""]);
+    const [page_count, setCount]  = React.useState(0);
+    const [cur_page, setPage] = React.useState(0);
 
-    componentDidMount(){
+    useEffect( () => {
         window.ipcRenderer.on(
             'nested-images-base64', (event:any, base64:string[]) => {
-                this.setState({image_urls: base64, total_images: base64.length})
+                setUrls(base64);
+                setCount(base64.length);
             }
         )
+    }, [])
 
-        // useKeyboardShortcut(['LeftArrow'], ()=>{});
-
-    }
-
-    selectDirectory = () => {
-        // alert(this.state.image_urls)
+    const selectDirectory = () => {
+        // alert(this.state.page_urls)
         window.ipcRenderer.send('open-dir-dialog');
     }
 
-
-    render() {
-        return (
-            <Box w="100%">
-                <Box h="32px" w={'100%'} bgColor="#343434"> 
-                    <Flex w={"100%"} h={"32px"} flexGrow={0} color="whiteAlpha.900">
-
-                        <TopMenuButton menuName="File">
-                            <ChildMenuButton name="Open Folder" onClick={()=>{this.selectDirectory()}}/>
-                            <ChildMenuButton name="Open Recent" onClick={()=>{this.selectDirectory()}}/>
-                            <ChildMenuButton name="Save Project" onClick={()=>{}}/> 
-                            <ChildMenuButton name="Exit project" onClick={()=>{}}/>
-                        </TopMenuButton>
-
-                        <TopMenuButton menuName="Edit"> 
-                        </TopMenuButton>
-                        
-                        <TopMenuButton menuName="View">
-                            <ChildMenuButton name="Fullscreen" onClick={()=>{}}/>
-                        </TopMenuButton>
-
-                        <TopMenuButton menuName="About" onClick={()=>{}}>
-                        </TopMenuButton>
-                    </Flex>
-
-                </Box>
-                {/* <img src={"./assets/temp/current-comic/000.jpg"} alt={'nope'} /> */}
-                {this.renderComicPage(0)}
-            </Box>
-        )
-    }
-
-    private renderComicPage(index:number){
+    const renderPage = () => {
         return (<>
-            {this.state.image_urls ? 
-                <img src={`data:image/png;base64,${this.state.image_urls[55]}`} alt={`NOT WORKING!`} /> : 
+            {page_urls ? 
+                <img src={`data:image/png;base64,${page_urls[cur_page]}`} alt={`NOT WORKING!`} /> : 
                 <>URL IS NULL!</>
             }
             </>
         )
     }
+
+    return (
+        <Box w="100%">
+            <Box h="32px" w={'100%'} bgColor="#343434"> 
+                <Flex w={"100%"} h={"32px"} flexGrow={0} color="whiteAlpha.900">
+
+                    <TopMenuButton menuName="File">
+                        <ChildMenuButton name="Open Folder" onClick={()=>{selectDirectory()}}/>
+                        <ChildMenuButton name="Open Recent" onClick={()=>{selectDirectory()}}/>
+                        <ChildMenuButton name="Save Project" onClick={()=>{}}/> 
+                        <ChildMenuButton name="Exit project" onClick={()=>{}}/>
+                    </TopMenuButton>
+
+                    <TopMenuButton menuName="Edit"> 
+                    </TopMenuButton>
+                    
+                    <TopMenuButton menuName="View">
+                        <ChildMenuButton name="Fullscreen" onClick={()=>{}}/>
+                    </TopMenuButton>
+
+                    <TopMenuButton menuName="About" onClick={()=>{}}>
+                    </TopMenuButton>
+                </Flex>
+
+            </Box>
+        {renderPage()}
+        <div> {`Current Page: ${cur_page} / ${page_count}`} </div>
+        </Box>
+    )
 }
