df53d18256522158d9527cf9531522c3c221b03f
Thodori Kapouranis <thodorikap@gmail.com>
2022-04-01 20:42:37 -0400

    Seperate region OCR to class

--- flask/main.py
+++ flask/main.py
@@ -1,5 +1,6 @@
 import io
 import json
+from typing import List
 from flask import Flask
 from flask import request
 from flask import Response
@@ -11,14 +12,14 @@ import torch
 import time
 import base64
 import pytesseract
+from OCR import OCR
 
 model = torch.jit.load("models/ts-model_final-cpu.pt")
 model.eval()
 ratioTransformer = ImageRatioTransform( shortest_length=800, max_length=1333)
-custom_fig = r'--oem 3 --psm 5'
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+ocr = OCR()
 
-def inferBoxes(model:any, transformer:any, img_data:any) -> list[list[int]]:
+def inferBoxes(model:any, transformer:any, img_data:any) -> List[List[int]]:
   '''
   Function that takes in inputted image, runs it through the transformer and into the inference model.
   Returns region in the form of [ [x1, y1, x2, y2], ...]
@@ -28,17 +29,18 @@ def inferBoxes(model:any, transformer:any, img_data:any) -> list[list[int]]:
   formatted_inputs = transformer.getTransform(img_data).apply_transform( tensor_format=True, device="cpu" )
   scale = formatted_inputs[1][0][2]
   pred = model(formatted_inputs)
-  boxes = pred[0] * scale
-  boxes = boxes.cpu().data.numpy()
+  regions = pred[0] * scale
+  regions = regions.cpu().data.numpy()
 
   json_arr = []
 
-  for i in boxes:
-    img_roi = img_data[int(i[1]-4):int(i[3]+4), int(i[0]-4):int(i[2]+4)]
-    ocr_result = pytesseract.image_to_string(img_roi, 'jpn_vert', config=custom_fig) # str
-    ocr_result = ocr_result.replace("\n", "")
-    ocr_result = ocr_result.replace(" ", "")
-    json_arr.append( {'xmin':int(i[0]), 'ymin':int(i[1]), 'xmax':int(i[2]), 'ymax':int(i[3]), 'text':ocr_result} )
+  for i in regions:
+    json_arr.append( ocr.recognizeRegion(img_data, i, padding=0) )
+    # img_roi = img_data[int(i[1]-4):int(i[3]+4), int(i[0]-4):int(i[2]+4)]
+    # ocr_result = pytesseract.image_to_string(img_roi, 'jpn_vert', config=custom_fig) # str
+    # ocr_result = ocr_result.replace("\n", "")
+    # ocr_result = ocr_result.replace(" ", "")
+    # json_arr.append( {'xmin':int(i[0]), 'ymin':int(i[1]), 'xmax':int(i[2]), 'ymax':int(i[3]), 'text':ocr_result} )
 
   t2 = time.time()
   print(t2-t1)
--- flask/ocr.py
+++ flask/ocr.py
@@ -1,22 +1,49 @@
+from typing import List
 import spacy
 from jamdict import Jamdict
+import pytesseract
 
 # https://github.com/neocl/jamdict/blob/main/jamdict/util.py
 
-if  __name__ == "__main__":
-  print("Finished imports")
-  nlp = spacy.load("ja_core_news_sm")
-  jam = Jamdict()
-  test = 'パチンコでとった金は大きか'
+#! Change this to the proper pytesseract bin file for your system.
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
-  doc = nlp(test)
-  for token in doc:
-    print(token.text, token.pos_, token.dep_)
+class OCR():
+  def __init__(self):
+    self.nlp = spacy.load("ja_core_news_sm")
+    self.custom_fig = r'--oem 3 --psm 5' # Pytesseract presets for vertical text detection
+  
+  def recognizeRegion(self, original_img, xyxy, padding:int=0) -> dict:
+    '''
+    Takes in a RGB array for an image and the region of interest (with padding) to perform vertical Japanese OCR.
+    Returns a JSON for the region with the xyxy region and text
+    '''
+    p = padding
+    left, top, right, down = int(xyxy[0]-p), int(xyxy[1]-p), int(xyxy[2]+p), int(xyxy[3]+p)
+    print(left, top, right, down)
+    img_roi = original_img[top:down, left:right]
+    print(img_roi.shape)
+
+    text = pytesseract.image_to_string(img_roi, 'jpn_vert', config=self.custom_fig) # str
+    text = text.replace("\n", "")
+    text = text.replace(" ", "")
+    print(text)
+    return {'xmin':left, 'ymin':top, 'xmax':right, 'ymax':down, 'text':text}
+
+# if  __name__ == "__main__":
+#   print("Finished imports")
+#   nlp = spacy.load("ja_core_news_sm")
+#   jam = Jamdict()
+#   test = 'パチンコでとった金は大きか'
+
+#   doc = nlp(test)
+#   for token in doc:
+#     print(token.text, token.pos_, token.dep_)
     
-    result = jam.lookup(token.text, strict_lookup=True)
-    for e in result.entries:
-      for s in e.senses:
-        print('\t', s)
-    print('\n')
+#     result = jam.lookup(token.text, strict_lookup=True)
+#     for e in result.entries:
+#       for s in e.senses:
+#         print('\t', s)
+#     print('\n')
 
 
